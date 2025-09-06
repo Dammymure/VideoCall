df1bb0f4953c398aac12368d12885dc92807180a
Dammy Mure <dammymure1@gmail.com>
2025-09-06 22:10:55 +0100

    fix video feed

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,25 +1,31 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from 'react';
 
 export default function VideoPlayer({ user, isMainVideo = false }) {
-    const ref = useRef()
-    
+    const ref = useRef();
+
     useEffect(() => {
-        user.videoTrack.play(ref.current)
-    }, [])
-    
-  return (
-    <div className="relative w-full h-full">
-        <div
-            ref={ref}
-            className={`w-full h-full object-cover ${
-                isMainVideo ? 'bg-gray-800' : 'bg-gray-700'
-            }`}
-        />
-        
-        {/* User label */}
-        <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded text-sm">
-            {user.isLocal ? 'You' : `User ${user.uid}`}
+        if (user && user.videoTrack && ref.current) {
+            user.videoTrack.play(ref.current);
+        }
+        return () => {
+            if (user && user.videoTrack) {
+                user.videoTrack.stop();
+            }
+        };
+    }, [user, user?.videoTrack]);
+
+    return (
+        <div className="relative w-full h-full">
+            <div
+                ref={ref}
+                className={`w-full h-full object-cover ${
+                    isMainVideo ? 'bg-gray-800' : 'bg-gray-700'
+                }`}
+            />
+            {/* User label */}
+            <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded text-sm">
+                {user.isLocal ? 'You' : `User ${user.uid}`}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
